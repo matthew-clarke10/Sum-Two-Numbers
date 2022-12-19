8393bbd3461e9945130ed976a3363c66ef23d49c
matthew-clarke10 <matthewkclarke10@gmail.com>
2022-12-19 17:45:43 +1100

    Finished Sum Project

--- sum.java
+++ sum.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
 
+public class Sum {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int A = scan.nextInt();
+        System.out.println("Now enter a second number: ");
+        int B = scan.nextInt();
+        System.out.println("The sum of these two numbers is " + (A+B) + ".");
+        System.exit(0);
+    }
+}
